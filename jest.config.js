5b535d4257c85fc5d322c951a0e909eb0d5d042b
themathmagician <agata@mathmagicians.dk>
2021-04-04 21:13:09 +0200

    RUN-21 #message result from lambda is displayed in frontend 😃

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,32 @@
 module.exports = {
+  preset: '@nuxt/test-utils',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['js', 'vue', 'json'],
+  moduleFileExtensions: [
+    'feature',
+    'js',
+    'vue',
+    'json'
+  ],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '.*\\.(vue)$': 'vue-jest',
+    '^.+\\.(feature)$': 'cucumber-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/store/**/*js'
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/node_modules/cucumber-jest/dist/init.js'
+  ],
+  testMatch: [
+    '<rootDir>/test/bdd/features/**/*.feature',
+    '<rootDir>/test/unit/**/*test.js'
   ]
 }
