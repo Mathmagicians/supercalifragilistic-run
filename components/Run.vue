45f16c459aa5e95afaf2d5656db206a27fd0156d
themathmagician <agata@mathmagicians.dk>
2021-04-10 17:04:43 +0200

    fixing ci on push to main

--- components/Run.vue
+++ components/Run.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <p>Run component</p>
+    <h2 class="text-2xl text-bold text-gray-800">
+      Run
+    </h2>
 
-    <div id="map-wrap" class="min-w-1/2 w-1/2" style="height: 100vh; width: 100vh;">
+    <div id="map-wrap" class="min-w-1/2 w-1/2" style="height: 50vw; width: 50vw; overflow: auto;">
       <client-only>
-        <l-map :zoom="13" :center="[55.9464418,8.1277591]">
+        <l-map :zoom="13" :center="center">
           <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
           <l-marker :lat-lng="[55.9464418,8.1277591]" />
         </l-map>
@@ -15,7 +17,12 @@
 
 <script>
 export default {
-  name: 'Run'
+  name: 'Run',
+  data () {
+    return {
+      center: '[41.279736, 0.723744]'
+    }
+  }
 }
 </script>
 
