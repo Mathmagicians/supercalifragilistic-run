4ea824b987646646daee808be6dfc165e9606114
themathmagician <agata@mathmagicians.dk>
2021-04-11 14:34:18 +0200

    Agatas run is displayed on map 🦩

--- components/Run.vue
+++ components/Run.vue
@@ -4,20 +4,29 @@
       Run
     </h2>
 
-    <div id="map-wrap" class="min-w-1/2 w-1/2" style="height: 50vw; width: 50vw; overflow: auto;">
+    <div id="map-wrap" class="min-w-1/2 w-1/2" style="height: 40vw; width: 40vw; overflow: auto;">
       <client-only>
-        <l-map :zoom="13" :center="center" @ready="setIconStyles">
-          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-          <l-marker :lat-lng="[center.lat, center.lng]" :icon="icon">
-            <l-popup> Hello, this is agatas run from {{ center }}</l-popup>
+        <l-map :zoom="15" :center="center" @ready="initMap">
+          <l-tile-layer
+            :url="url"
+          />
+          <l-marker
+            :lat-lng="[center.lat, center.lng]"
+            :icon="icon"
+          >
+            <l-popup> Hello, this is a star on Agatas run from {{ center }}</l-popup>
           </l-marker>
-          <l-polyline :latlngs="polyline" @ready="decode" />
+          <l-polyline :lat-lngs="polyline.latlngs" :color="polyline.color" />
         </l-map>
       </client-only>
     </div>
 
-    <p>Polyline</p>
-    <p>{{ polyline }}</p>
+    <p class="text-xs">
+      Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a
+        href="https://www.flaticon.com/"
+        title="Flaticon"
+      >www.flaticon.com</a>
+    </p>
   </div>
 </template>
 
@@ -28,8 +37,12 @@ export default {
   name: 'Run',
   data () {
     return {
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
       icon: null,
-      polyline: null,
+      polyline: {
+        latlngs: [],
+        color: 'DeepPink'
+      },
       center: {
         lat: 41.279736,
         lng: 0.723744
@@ -37,16 +50,21 @@ export default {
     }
   },
   methods: {
+
+    initMap () {
+      this.setIconStyles()
+      this.decode()
+    },
+
     setIconStyles () {
       this.icon = this.$L.icon({
-        iconUrl: 'https://www.freeiconspng.com/uploads/star-icon-32.png'
+        iconUrl: require('~/assets/bw_star_marker.png')
       })
     },
 
     decode () {
       const encoded = 'im}zFkjlCWpADBGHLBCUISIK@FCGKI[e@Om@KUC[EU@CJHCc@GM?Y@@@ABHALHB\\\\GNAPGNSZQf@Af@ICDDA?BNA?HLPXDl@Qb@UHBVn@NVRR`@HJFpBnC^^LXHh@JVVZVBNHdABRD`A@h@C@?C@D?fAe@ZELBPNTFf@A`@JFANDDARJl@p@Pj@HFLAHDHL@NAKDCBVRp@A@JTRTVPZh@BHFD^p@@DCL@RDV?NRLHLLh@HJRh@JNZx@Hp@BH?PCRCEDa@DM@KGQ?QK_@m@{AWw@U_@KW@WKe@O_@MMc@y@gAqAEM?IQ]C?SOIC_@o@IEa@e@_@OMA?@MGC@M?A@E?CDQKm@SKMG?M?GB?AIHOBs@\\\\UA_@DUEI@q@AI@ME_@A_@GC@E?UOIQOs@We@q@y@}@wAIGU]SGIAWMa@k@O]@QDCLWGSIA[Ha@@GBONKDICGKI_@Eu@YaBAUDSHgBHe@@k@TwAH[Hs@b@m@Ty@HMRk@l@}@E?EBGLWRGJ]|@Sv@MTMJEJGZK~@M`@Ij@S|BEz@En@Z`BLfADNHNAHEBIJq@FO?KBc@AWP[DEDUBOFE@OICB?FHHEJ?LCDDFABGCAH@XD`@AL@PDF@LHJ@HDJf@j@DCB@@JABHF?D?CMOm@_@IFE@BHCFO?Qb@MPOx@Iz@FPBA@DAEE@r@R'
-      const latlngs = polyUtil.decode(encoded)
-      this.polyline = latlngs
+      this.polyline.latlngs = polyUtil.decode(encoded)
     }
   }
 }
