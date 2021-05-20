303c0b3b99f7fb3cd6c6143053a67d2ad3e23ffe
themathmagician <agata@mathmagicians.dk>
2021-05-20 20:16:05 +0200

    Polyline (#64)
    * Can show stars on map with runs
    
    * Leaderboard cleaned up
    
    * Added start and end date to challenge page

--- pages/challenge.vue
+++ pages/challenge.vue
@@ -1,6 +1,6 @@
 <template>
   <basic-page-layout v-if="!$fetchState.pending">
-    <leaderboard v-for="challenge in list" :key="challenge.id" :is-mine="isMine(challenge.id)" :challenge="challenge" />
+    <leaderboard />
   </basic-page-layout>
   <div v-else>
     Wait while we load data ...
@@ -12,19 +12,17 @@ import Leaderboard from '../components/Leaderboard'
 import BasicPageLayout from '../components/layout-utils/BasicPageLayout'
 export default {
   components: { BasicPageLayout, Leaderboard },
-  computed: {
-    ...mapGetters({ list: 'challenge/list' })
+  async fetch () {
+    const { store } = this.$nuxt.context
+    console.info('[challenge] loading page,')
+    await store.dispatch('handleUserLogin')
+    await store.dispatch('challenge/fetchMyChallenge', store.state.profile.my_challenges)
   },
   methods: {
     isMine (id) {
       const included = true // this.myChallenges.includes(id)
       return included
     }
-  },
-  async fetch () {
-    const { store } = this.$nuxt.context
-    console.info('[challenge] loading page,')
-    await this.$store.dispatch('challenge/fetchChallenge')
   }
 }
 </script>
--- store/challenge.js
+++ store/challenge.js
@@ -1,57 +1,63 @@
 export const state = () => ({
-
-  athletes: []
+  myChallenge: {
+    Athletes: []
+  }
 })
 
 export const getters = {
-  getAthletes: (state) => { return state.athletes }
+  getAthletes: (state) => { return state.athletes },
+  getMyChallenge: (state) => { return state.myChallenge }
 }
 
 export const mutations = {
-  setChallenge (state, athletes) {
-    const filterRunsOver3Km = r => r >= 3000.0
-    const runCountReducer = (acc, r) => acc + 1
-    const distanceCountReducer = (acc, r) => acc + r
-    // we want reverse order
-    const compareByTotalScore = (a, b) => b.scores.total - a.scores.total
-
-    console.log(`[setChallenge] received data for ${athletes.length} athletes`, athletes)
-
-    athletes.forEach((a) => {
-      a.scores = {}
-      console.log(`[setChallenge] about to compute scores for athlete ${a.id}`)
-      a.scores.startBonus = 3 * (a.runs ? a.runs.map(r => r.distance).filter(filterRunsOver3Km).reduce(runCountReducer, 0) : 0)
-      a.scores.distanceTotal = (a.runs ? a.runs.map(r => r.distance).filter(filterRunsOver3Km).reduce(distanceCountReducer, 0) : 0) / 1000
-
-      a.scores.total = a.scores.startBonus + a.scores.distanceTotal
-      console.log(`[setChallenge] interim computation of scores for athlete ${a.id}:`, a.scores)
-    })
-    athletes.sort(compareByTotalScore)
-    console.log('[setChallenge] sorted data:', athletes)
-    state.athletes = [...athletes]
-  },
-
   setMyChallenge (state, myChallenge) {
     state.myChallenge = myChallenge
   },
 
   setChallenges (state, challenges) {
     state.challenges = [...challenges]
+  },
+  setAthletes (state, myChallenge) {
+    const filterRunsOver3Km = r => r >= 3000.0
+    const runCountReducer = (acc, r) => acc + 1
+    const distanceCountReducer = (acc, r) => acc + r
+    // we want reverse order
+    const compareByTotalScore = (a, b) => b.Score.Total - a.Score.Total
+
+    const athletes = myChallenge.Athletes
+    console.log(`[computeInterimScores] received data for ${athletes?.length} athletes`)
+
+    athletes.forEach((a) => {
+      console.log(`[computeInterimScores] about to compute scores for athlete ${a.ProfileId}`)
+      a.Score.StartBonus = 3 * (a.Runs ? a.Runs.map(r => r.distance).filter(filterRunsOver3Km).reduce(runCountReducer, 0) : 0)
+      a.Score.Kilometer = (a.Runs ? a.Runs.map(r => r.distance).filter(filterRunsOver3Km).reduce(distanceCountReducer, 0) : 0) / 1000
+
+      a.Score.Total = a.Score.StartBonus + a.Score.Kilometer
+      console.log(`[computeInterimScores] interim computation of scores for athlete ${a.id}:`, a.Score.Total)
+    })
+    athletes.sort(compareByTotalScore)
+    console.log('[computeInterimScores] sorted data')
+    state.myChallenge.Athletes = [...athletes]
   }
 }
 
 export const actions = {
 
-  async fetchChallenge ({ state, commit }) {
-    console.warn('[fetchChallenge] fetching challenge - hardwired to /profile')
-    // fixme call /challenge/id instead
+  async fetchMyChallenge ({ commit, state, dispatch }, mych) {
+    console.log('[FetchMyChallenge] ready to fetch challenge:', mych)
+    if (!mych?.length) {
+      console.warn('Not participating in any challenge')
+      return
+    }
     try {
       const get = await this.$axios({
         method: 'get',
-        url: '/profile'
+        url: `/challenge/${mych[0]}`
       })
-      console.info(`[fetchChallenges] fetched athlete stats, status: ${status}`, get.status)
-      commit('setChallenge', get.data)
+      console.log('[FetchMyChallenge] received data from server')
+      commit('setMyChallenge', get.data)
+      commit('setAthletes', get.data)
+      console.log('[FetchMyChallenge] Challenge data fetched for athlete')
     } catch (error) {
       // if status code is 404 return as value
       if (error.response && error.response.status === 404) {
@@ -63,23 +69,6 @@ export const actions = {
         console.error('[fetchChallenge] : Cant communicate with backend')
         throw (error)
       }
-    }
-  },
-
-  async fetchMyChallenge ({ commit, state }, mych) {
-    if (!mych?.length) {
-      console.warn('Not participating in any challenge')
-      return
-    }
-    try {
-      const get = await this.$axios({
-        method: 'get',
-        url: `/challenge/${mych[0]}`
-      })
-      commit('setMyChallenge', get.data)
-      console.log('[FetchMyChallenge] Challenge data fetched for athlete')
-    } catch (error) {
-      console.error('[fetchChallenge]: Uh, oh, somethings wrong with the response')
     }
   },
 
