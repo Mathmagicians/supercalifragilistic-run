fc97469e06b8be065bc5a4bbca46151f1a94f346
themathmagician <agata@mathmagicians.dk>
2021-05-26 15:51:51 +0200

    added day seperator to my runs

--- components/myRuns.vue
+++ components/myRuns.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="w-full ">
     <page-section-title>
-      Your runs
+      {{ myAthlete.Meta.Fav }} {{ myAthlete.Meta.Name }}'s Running Efforts
     </page-section-title>
-    <p class="text-xs text-gray-400">
-      Data fetched {{ latest_fetch| timeSince }}
+    <p class="text-xs text-gray-400 ">
+      Data fetched {{ myAthlete.Meta.LatestFetch | timeSince }}
     </p>
+    <leaderboard :athletes="[myAthlete]" />
     <div class="flex flex-col sm:flex-row flex-wrap">
-      <run
-        v-for="(aRun, key) in runs"
-        :key="aRun.id"
-        :run="aRun"
-        :number="runs.length - key"
-        class="w-1/2"
-      />
+      <div v-for="(e, k) in myAthlete.Events" :key="e.Date">
+        <div class="bg-gray-900 text-white text-3xl my-4 text-current p-overflow-hidden">
+          <h2 class="p-2">
+            <CalendarIcon size="1.5x" class="inline-block" /> Run Day {{ myAthlete.Events.length - k }} ✨ {{ e.Date }}
+            ✨  <LightningBoltIcon class="inline-block" /> Runs {{ e.Runs.length }}
+            ✨ <CalculatorIcon size="1.5x" class="inline-block" />
+            Score {{ (e.Score.Total).toFixed(0) }}
+            ✨ <MapIcon size="1.5x" class="inline-block" />
+            Distance {{ (e.Score.KmsTotal).toFixed(1) }} km
+            ✨ <StarIcon size="1.5x" class="inline-block" />
+            Stars {{ e.Score.StarsCollected }}
+          </h2>
+        </div>
+        <run
+          v-for="(aRun, j) in e.Runs"
+          :key="aRun.id"
+          :run="aRun.Run"
+          :star-visits="aRun.StarVisits"
+          :run-value="aRun.RunValue"
+          :number="[myAthlete.Events.length - k, e.Runs.length - j]"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { StarIcon, MapIcon, CalendarIcon, LightningBoltIcon, UserIcon, CloudDownloadIcon, CalculatorIcon, ClipboardListIcon } from '@vue-hero-icons/outline'
 import PageSectionTitle from './layout-utils/PageSectionTitle'
 
 export default {
   name: 'MyRuns',
-  components: { PageSectionTitle },
+  components: { PageSectionTitle, CalendarIcon, LightningBoltIcon, MapIcon, CalculatorIcon, StarIcon },
   computed: {
-    ...mapGetters(['runs', 'latest_fetch'])
+    ...mapGetters({
+      myAthlete: 'challenge/getMyAthlete'
+    })
+
   }
 }
 </script>
--- store/challenge.js
+++ store/challenge.js
@@ -1,12 +1,18 @@
 export const state = () => ({
   myChallenge: {
     Athletes: []
-  }
+  },
+  myAthlete: {}
 })
 
 export const getters = {
-  getAthletes: (state) => { return state.athletes },
-  getMyChallenge: (state) => { return state.myChallenge }
+  getAthletes: (state) => { return state.myChallenge.Athletes },
+  getMyChallenge: (state) => { return state.myChallenge },
+  getMyAthlete: (state) => { return state.myAthlete },
+  getAthletePosition: state => (id) => {
+    const matchId = athlete => athlete.ProfileId === id
+    return state.myChallenge.Athletes.findIndex(matchId) + 1
+  }
 }
 
 export const mutations = {
@@ -16,10 +22,47 @@ export const mutations = {
 
   setChallenges (state, challenges) {
     state.challenges = [...challenges]
+  },
+
+  setMyAthlete (state, athlete) {
+    state.myAthlete = { ...athlete }
   }
 }
 
 export const actions = {
+
+  async fetchMyAthlete ({ commit, state, getters, dispatch }, { athlete, challenges }) {
+    console.log('[fetchMyAthlete] fetching athlete data for logged in profile, athlete', athlete, challenges)
+    if (!challenges?.length > 0) {
+      console.warn(`[fetchAthlete] Received invalid parameters athlete:${athlete}, challenge: ${challenges}`)
+      return
+    }
+    const challenge = challenges[0]
+    const myAthlete = await dispatch('fetchAthlete', { athlete, challenge })
+    if (myAthlete) {
+      commit('setMyAthlete', myAthlete)
+    }
+  },
+
+  async fetchAthlete ({ commit, state, dispatch }, { athlete, challenge }) {
+    console.log(`[fetchAthlete] fetching athlete data for athlete ${athlete} in challenge ${challenge}`)
+    if (!athlete?.length || !challenge?.length) {
+      console.warn(`[fetchAthlete] Received invalid parameters athlete:${athlete}, challenge: ${challenge}`)
+      return
+    }
+    try {
+      const get = await this.$axios({
+        method: 'get',
+        url: `/athlete/${athlete}?challenge=${challenge}`
+      })
+      console.log('[FetchAthlete] received data from server')
+      commit('setMyAthlete', get.data)
+      console.log('[FetchMyAthlete] Challenge data fetched for athlete')
+      return get.data
+    } catch (error) {
+
+    }
+  },
 
   async fetchMyChallenge ({ commit, state, dispatch }, mych) {
     console.log('[FetchMyChallenge] ready to fetch challenge:', mych)
