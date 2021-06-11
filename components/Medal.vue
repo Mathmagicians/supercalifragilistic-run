9dd31952d64fdda806687ef49f80c44a5212d7a9
themathmagician <agata@mathmagicians.dk>
2021-06-12 01:21:57 +0200

    Found a solution to color problem

--- components/Medal.vue
+++ components/Medal.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex flex-col items-center mx-2 sm:mx-5 w-1/8">
     <div
-      class="rounded-full border-4 ring-4 p-6 sm:p-12 mb-2 transform transition hover:scale-105 hover:-translate-y-20 duration-500 ease-in-out"
-      :class="medal"
+      class="overflow-hidden rounded-full border-4 ring-4 p-6 sm:p-12 mb-2 transform transition hover:scale-125 hover:-rotate-12 hover:-translate-y-20 duration-500 ease-in-out bg-gradient-to-tr"
+      :class="`${mp.mg} ${mp.text} ${mp.border} ${mp.ring}`"
     >
       <component
         :is="mp.image"
-        class="inline-block fill-current text-md font-extrabold w-24 h-24 sm:w-48 sm:h-48 p-4 bg-gradient-to-tr"
-        :class="textColor"
+        class="inline-block fill-current text-md font-extrabold w-24 h-24 sm:w-48 sm:h-48 p-4 "
+        :class="`${textColor}`"
       />
     </div>
 
     <div
       v-if="withPodium"
-      class="border-4 rounded-lg shadow-lg w-full bg-gradient-to-t"
-      :class="podium"
+      class="border-4 rounded-lg shadow-lg w-full bg-gradient-to-t from-pink-200 via-pink-100 to-white"
+      :class="mp.text"
     >
       <ul class="text-center py-2">
         <li class="text-2xl sm:text-6xl" :class="textColor">
@@ -45,6 +45,10 @@ import Overtaking from '~/assets/Overtaking.svg?inline'
 const medalProperties = {
   1: {
     color: 'yellow-400',
+    mg: 'from-yellow-100 via-white to-yellow-400',
+    text: 'text-yellow-400',
+    border: 'border-yellow-400',
+    ring: 'ring-yellow-200',
     image: 'First',
     podiumHeight: 5,
     podiumTextTop: '1st',
@@ -52,6 +56,10 @@ const medalProperties = {
   },
   2: {
     color: 'gray-400',
+    mg: 'from-gray-100 via-white to-gray-400',
+    text: 'text-gray-400',
+    border: 'border-gray-400',
+    ring: 'ring-gray-200',
     image: 'Second',
     podiumHeight: 3,
     podiumTextTop: '2nd',
@@ -59,6 +67,10 @@ const medalProperties = {
   },
   3: {
     color: 'yellow-700',
+    mg: 'from-yellow-500 via-white to-yellow-700',
+    text: 'text-yellow-700',
+    border: 'border-yellow-700',
+    ring: 'ring-yellow-200',
     image: 'Third',
     podiumHeight: 2,
     podiumTextTop: '3rd',
@@ -66,6 +78,10 @@ const medalProperties = {
   },
   AllStars: {
     color: 'pink-400',
+    mg: 'from-pink-100 via-white to-pink-400',
+    text: 'text-pink-400',
+    border: 'border-pink-400',
+    ring: 'ring-pink-100',
     image: 'Allstars',
     podiumTextTop: '⭐️',
     podiumTextMiddle: 'collected all stars'
@@ -74,6 +90,10 @@ const medalProperties = {
   MostStarsWeek: {},
   Default: {
     color: 'pink-400',
+    mg: 'from-pink-100 via-white to-pink-400',
+    text: 'text-pink-400',
+    border: 'border-pink-400',
+    ring: 'ring-pink-100',
     image: 'Overtaking',
     podiumTextTop: '🦩',
     podiumTextMiddle: 'Awarded for participation'
@@ -101,31 +121,8 @@ export default {
     mp () {
       return medalProperties[this.for] ? medalProperties[this.for] : medalProperties.Default
     },
-    baseColor () {
-      return this.mp.color.split('-')[0]
-    },
-    medal () {
-      return `${this.textColor} ${this.borderColor} ${this.ringColor}`
-    },
-    podium () {
-      // return `from-${this.baseColor}-200 via-${this.baseColor}-100 to-white border-${this.mp.color} ${this.textColor}`
-      return `from-pink-200 via-pink-100 to-white ${this.textColor}`
-    },
     podiumHeight () {
       return this.mp?.podiumHeight ? this.mp.podiumHeight : 1
-    },
-    medalGradient () {
-      return `from-${this.mp.color} via-white to-${this.mp.color}`
-    },
-    textColor () {
-      return `text-${this.mp.color}`
-    },
-    borderColor () {
-      return `border-${this.mp.color}`
-    },
-    ringColor () {
-      const base = this.mp.color.split('-')[0]
-      return `ring-${this.mp.color}`
     }
   }
 
